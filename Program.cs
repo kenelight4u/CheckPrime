d29f84d491cc4264981fdc30de5fc3fb97393bba
Anolue Ekene Samuel <kenelight4u@gmail.com>
2021-04-20 11:15:33 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -6,31 +6,23 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            int add = 0;
-            for (int i = 1; i <= 10; i++)
-            {
-                add = add + i;
-            }
-            Console.WriteLine(add);
+            // int add = 0;
+            // for (int i = 1; i <= 10; i++)
+            // {
+            //     add = add + i;
+            // }
+            // Console.WriteLine(add);
 
-            // Console.WriteLine("\nCHECK FOR A PRIME NUMBER \n");
-            // Console.Write("Enter the Number: ");
-            // long number = long.Parse(Console.ReadLine());
+            Console.WriteLine("\nCHECK FOR A PRIME NUMBER \n");
+            Console.Write("Enter the Number: ");
+            long number = long.Parse(Console.ReadLine());
 
-            // if (IsPrime(number))
-            //     Console.WriteLine("Prime Number");               
-            // else
-            //     Console.WriteLine("Not a Prime Number");
+            if (IsPrime(number))
+                Console.WriteLine("Prime Number");               
+            else
+                Console.WriteLine("Not a Prime Number");
 
-            // Console.WriteLine("An App to check if a number is Prime number");
-            // Console.ReadLine();
-            // Console.WriteLine("Enter number");
-            // int number=Convert.ToInt32(Console.ReadLine());
-            // bool result=Check_Prime(number);
-            // if(result==false){
-            // Console.WriteLine($"{number} is not a prime number");
         }
-
         public static bool IsPrime(long number) {
             if (number <= 1)
                 return false;
